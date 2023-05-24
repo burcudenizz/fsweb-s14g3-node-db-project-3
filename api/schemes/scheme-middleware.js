034a87d9058c5de2f8c39e35475d339768cbf01b
Burcu Deniz <dnzzburcu@gmail.com>
2023-05-24 15:32:59 +0300

    scheme-middleware tamamlandı.

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -6,9 +6,27 @@
     "message": "scheme_id <gerçek id> id li şema bulunamadı"
   }
 */
-const checkSchemeId = (req, res, next) => {
 
-}
+const db = require("../../data/db-config");
+
+const checkSchemeId = async (req, res, next) => {
+  try {
+    const isExist = await db("schemes")
+      .where("schema_id", req.params.scheme_id)
+      .first();
+
+    if (!isExist) {
+      res.status(404).json({
+        message: `scheme_id ${req.params.scheme_id} id li şema bulunamadı`,
+      });
+    } else {
+      req.scheme = isExist;
+      next();
+    }
+  } catch (error) {
+    next(error);
+  }
+}; // veritabanına gittiğimiz için async/await yaparız.
 
 /*
   Eğer `scheme_name` yoksa, boş string ya da string değil:
@@ -18,8 +36,22 @@ const checkSchemeId = (req, res, next) => {
     "message": "Geçersiz scheme_name"
   }
 */
-const validateScheme = (req, res, next) => {
+const validateScheme = (req, res, next) => {};
 
+try {
+  const { scheme_name } = req.body;
+
+  if (
+    scheme_name === undefined ||
+    typeof scheme_name !== "string" ||
+    scheme_name.trim() === ""
+  ) {
+    res.status(400).json({ message: "Geçersiz scheme_name" });
+  } else {
+    next();
+  }
+} catch (error) {
+  next(error);
 }
 
 /*
@@ -32,11 +64,26 @@ const validateScheme = (req, res, next) => {
   }
 */
 const validateStep = (req, res, next) => {
-
-}
+  try {
+    const { instructions, step_number } = req.body;
+    if (
+      instructions === undefined ||
+      typeof instructions !== "string" ||
+      instructions.trim() == "" ||
+      typeof step_number !== "number" ||
+      step_number < 1
+    ) {
+      res.status(400).json({ message: "Hatalı step" });
+    } else {
+      next();
+    }
+  } catch (error) {
+    next(error);
+  }
+};
 
 module.exports = {
   checkSchemeId,
   validateScheme,
   validateStep,
-}
+};
